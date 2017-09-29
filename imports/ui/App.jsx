39bcd2a026841826df9e0c3aae9696b9ba29244a
colinbinion <colin.binion@me.com>
2017-09-29 16:00:35 -0600

    adding collections for DB

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import{ Tasks } from '../api/tasks.js'
 
 import Task from './Task.jsx';
 
 //represents whole app - app Component
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
-
+class App extends Component {
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -23,6 +19,14 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -32,3 +36,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.PropTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
